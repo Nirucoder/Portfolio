c03124e089420990a94c42fd09b3ae6b9921ec78
nirattay183@gmail.com <nirattay183@gmail.com>
2026-06-20 12:17:15 +0530

    Update resume link

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1GSACc3msI0I8gvoRgrR6uzv5aQBeIey6/view?usp=sharing"
+                    "https://drive.google.com/file/d/1O7ggPXoMIed_b44ibNF_k8wd4HA8i-N1/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
